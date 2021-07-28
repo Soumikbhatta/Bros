5816624cc6d8f4bcd8be4e9e200d18abbd99de3e
Soumikbhatta <soumikbhatta@gmail.com>
2021-07-28 22:29:39 +0530

    app bar styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, AppBar, Grow, Grid } from '@material-ui/core';
+//removed Typography for now
 
 import Bros from './images/bros.png';
 
 import Posts from './components/posts/posts';
 import Form from './components/form/form';
 
+import useStyles from './styles';
+
 const App = () =>{
+    const classes = useStyles();
+
     return(
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Bros</Typography>
-                <img src={Bros} alt="Bros" width="600px" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <img className={classes.image} src={Bros} alt="Bros" width="15%" />
+                {/* <Typography variant="h2" align="center">A Social media web app</Typography> */}
             </AppBar>
             <Grow in>
                     <Container>
