af3590ec879b095103efd6956e13a794e76f9cd9
Soumikbhatta <soumikbhatta@gmail.com>
2021-08-23 05:15:09 +0530

    update post route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -24,3 +24,11 @@ export const createPost =  async (req,res) => {
         res.status(409).json({ message: error.message });        
     }
 }
+
+export const updatePost = async (req,res) => {
+    const { id:_id } = req.params;
+    const post = req.body
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('Post id not found');
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new:true });
+    res.json(updatedPost);
+}
