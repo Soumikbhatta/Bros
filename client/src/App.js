64419d81a2bfd8643e76c938ce193c0d2cd49e5e
Soumikbhatta <soumikbhatta@gmail.com>
2021-08-23 03:59:38 +0530

    changed to dark theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Container, AppBar, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 //removed Typography for now
 
 import { getPosts } from './actions/posts';
@@ -11,6 +12,7 @@ import Posts from './components/posts/posts';
 import Form from './components/form/form';
 
 import useStyles from './styles';
+import { grey } from '@material-ui/core/colors';
 
 const App = () =>{
     const classes = useStyles();
@@ -20,8 +22,22 @@ const App = () =>{
         dispatch(getPosts());
     },[dispatch]);
 
+    const theme = createTheme({
+        palette: {
+            type: 'dark',
+            primary: {
+                main: 'rgb(255, 255, 255)'
+            },
+            secondary: grey,
+            background: {
+                paper: 'rgb(26, 26, 26)'
+            }
+        }
+    })
+
     return(
-        <Container className={classes.body} maxWidth="lg">
+        <ThemeProvider theme={theme}>
+            <Container className={classes.body} maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
                 <img className={classes.image} src={Bros} alt="Bros" width="200vw" />
                 {/* <Typography variant="h2" align="center">A Social media web app</Typography> */}
@@ -46,6 +62,9 @@ const App = () =>{
                 </Grow>
 
         </Container>
+
+        </ThemeProvider>
+    
     )
 }
 
