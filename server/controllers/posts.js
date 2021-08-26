03c4a83f295b28b7e5b2a42512b49c21afd07e4f
Soumikbhatta <soumikbhatta@gmail.com>
2021-08-27 01:41:25 +0530

    like post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -17,9 +17,7 @@ export const createPost =  async (req,res) => {
     const newPost = new PostMessage(post);
     try {
         await newPost.save();
-
         //https://www.restapitutorial.com/httpstatuscodes.html
-
         res.status(201).json(newPost);        
     } catch (error) {
         res.status(409).json({ message: error.message });        
@@ -30,8 +28,6 @@ export const updatePost = async (req,res) => {
     const { id: _id } = req.params;
     const post = req.body;
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('Post id not found');
-
-
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
     res.json(updatedPost);
 }
@@ -39,8 +35,14 @@ export const updatePost = async (req,res) => {
 export const deletePost = async (req, res) => {
     const { id } = req.params;
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('Post id not found');
-
     await PostMessage.findByIdAndRemove(id);
-
     res.json({ message: 'Post deleted successfully'})
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+    const post = await PostMessage.findById(id);
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('Post id not found');
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount : post.likeCount + 1 }, { new: true});
+    res.json(updatedPost);
 }
