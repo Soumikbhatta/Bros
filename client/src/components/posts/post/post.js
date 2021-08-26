8c8134ab037ea1c9d6b84865c6910e5b6488aa73
Soumikbhatta <soumikbhatta@gmail.com>
2021-08-27 00:40:24 +0530

    delete post

--- client/src/components/posts/post/post.js
+++ client/src/components/posts/post/post.js
@@ -5,9 +5,13 @@ import DeleteICon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
     return(
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile || 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'} title={post.title} />
@@ -33,7 +37,7 @@ const Post = ({ post, setCurrentId }) => {
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onCLick={() =>{}}>
+                <Button size="small" color="primary" onCLick={() =>{dispatch(deletePost(post._id))}}>
                     <DeleteICon fontSize="small" />
                     Delete
                 </Button>
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,7 +2,7 @@
 export default (posts = [], action) => {
     switch (action.type) {
         case 'DELETE':
-            return posts.filter();
+            return posts.filter((post) => post._id !== action.payload);
         case 'UPDATE':
             return posts.map((post)=> post._id === action.payload._id ? action.payload : post );
         case 'FETCH_ALL':
@@ -13,5 +13,5 @@ export default (posts = [], action) => {
             return posts;
     }
 
-};
+}; 
 
