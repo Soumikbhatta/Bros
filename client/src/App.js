06ed88d7fca387b116f1eab1c809531094643fac
Soumikbhatta <soumikbhatta@gmail.com>
2021-09-08 04:11:15 +0530

    auth form

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { grey } from '@material-ui/core/colors';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth'
 
 const App = () => {
   const theme = createTheme({
@@ -36,7 +37,7 @@ const App = () => {
           <Navbar />
           <Switch>
             <Route path='/' exact component={Home} />
-            {/* <Route path='/auth' exact component={Auth} /> */}
+            <Route path='/auth' exact component={Auth} />
           </Switch>
         </Container>
       </BrowserRouter>
